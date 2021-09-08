e342b3e56ccdd1d97c189d3441ac06655f80515c
intns <84647527+intns@users.noreply.github.com>
2021-09-08 18:03:55 +0100

    Release 1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Text-Search
 A tiny program made for searching text files Notepad++ doesn't want to
+
+## Usage
+To input the file you want to search, you may drag and drop the text file (or provide the file using a command line) on the executable, if no files were provided on the command line the program will ask for a path to the file.
+
+Once you have entered a file successfully and it has been opened (no errors will have occured), it will ask for the text you want to search for. Once it begins searching if it is found then it will return the line number that matches, and the line itself.
--- main.cpp
+++ main.cpp
@@ -1,52 +1,53 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <conio.h>
-
-#ifdef _MSC_VER
-#define getch() _getch()
-#endif
+#include <fstream>
+#include <iostream>
+#include <string>
 
 static void handleExit()
 {
-	std::printf("Press any key to exit");
-	int c = getch();
+    std::cout << "Press any key to exit";
+
+#ifdef _MSC_VER
+    ((void)_getch());
+#else
+    ((void)getch());
+#endif
 }
 
 int main(int argc, char** argv)
 {
-	std::atexit(handleExit);
+    std::atexit(handleExit);
 
-	std::string fileName = "";
-	if (argc == 2)
-	{
-		fileName = argv[1];
-	}
-	else
-	{
-		std::printf("Enter file to search: ");
-		std::getline(std::cin, fileName);
-	}
+    std::string fileName = "";
+    if (argc == 2) {
+        fileName = argv[1];
+    } else {
+        std::cout << "Enter file to search: ";
+        std::getline(std::cin, fileName);
+    }
 
-	std::printf("Enter text to look for: ");
-	std::string searchFor = "";
-	std::getline(std::cin, searchFor);
+    std::cout << "Enter text to look for: ";
+    std::string searchFor = "";
+    std::getline(std::cin, searchFor);
 
-	std::ifstream file(fileName);
-	if (!file.is_open())
-	{
-		std::printf("Unable to open file\n");
-		return EXIT_FAILURE;
-	}
+    std::ifstream file(fileName);
+    if (!file.is_open()) {
+        std::cout << "Unable to open file" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-	std::string line = "";
-	for (std::size_t idx = 1; std::getline(file, line); idx++)
-	{
-		if (line.find(searchFor) != std::string::npos)
-		{
-			std::printf("Found on line %d: %s\n", idx, line.c_str());
-		}
-	}
+    std::string line = "";
+    bool found       = false;
+    for (std::size_t idx = 1; std::getline(file, line); idx++) {
+        if (line.find(searchFor) != std::string::npos) {
+            found = true;
+            std::cout << "Found on line " << idx << " : " << line << std::endl;
+        }
+    }
 
-	return EXIT_SUCCESS;
+    if (!found) {
+        std::cout << "Unable to find wanted text" << std::endl;
+    }
+
+    return EXIT_SUCCESS;
 }
